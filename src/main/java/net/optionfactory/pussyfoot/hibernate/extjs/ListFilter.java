312c92593fb7cd30b948af006b7e16213bb048b4
Simone Baruzza <simone.baruzza@optionfactory.net>
2018-09-12 15:33:10 +0200

    :enh: add support for null values in ListFilter

--- src/main/java/net/optionfactory/pussyfoot/hibernate/extjs/ListFilter.java
+++ src/main/java/net/optionfactory/pussyfoot/hibernate/extjs/ListFilter.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.List;
 import java.util.function.BiFunction;
+import java.util.stream.Collectors;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Predicate;
@@ -23,8 +24,19 @@ public class ListFilter<TRoot, T> implements JpaFilter<TRoot, String> {
     @Override
     public Predicate predicateFor(CriteriaBuilder cb, Root<TRoot> root, String value) {
         try {
-            final List listUnmarshalled = objectMapper.readValue(value, List.class);
-            return path.apply(cb, root).in(listUnmarshalled);
+            final List<?> listUnmarshalled = objectMapper.readValue(value, List.class);
+
+            if (!listUnmarshalled.contains(null)) {
+                return path.apply(cb, root).in(listUnmarshalled);
+            }
+            final List<?> withoutNulls = listUnmarshalled.stream().filter(i -> i != null).collect(Collectors.toList());
+            if (withoutNulls.isEmpty()) {
+                return path.apply(cb, root).isNull();
+            }
+            return cb.or(
+                    path.apply(cb, root).in(withoutNulls),
+                    path.apply(cb, root).isNull()
+            );
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
