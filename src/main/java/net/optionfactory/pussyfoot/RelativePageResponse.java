63e9e935c8c26e10f3cbbf95b49fb80ce16fffae
Roberto Ferranti <roberto.ferranti@gmail.com>
2024-02-12 10:56:22 +0100

    :new: bumped dependencies

--- src/main/java/net/optionfactory/pussyfoot/RelativePageResponse.java
+++ src/main/java/net/optionfactory/pussyfoot/RelativePageResponse.java
@@ -1,7 +1,6 @@
 package net.optionfactory.pussyfoot;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
